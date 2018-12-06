757b20f1a48684f0a6d8d670c210fbccd40b9695
Sean Feldman <feldman.sean@gmail.com>
2018-12-05 22:06:01 -0700

    Initial version - throws wrong exception if port 5671 is blocked from the get go

---
+++ ReproApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ReproApp
+{
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Azure.ServiceBus.Core;
+    using Microsoft.Azure.ServiceBus.Management;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+
+            var management = new ManagementClient(connectionString);
+
+            if (!await management.QueueExistsAsync("queue"))
+            {
+                await management.CreateQueueAsync("queue");
+            }
+
+            var sender = new MessageSender(connectionString, "queue");
+
+            while (true)
+            {
+                using (var tx = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    await sender.SendAsync(new Message(Encoding.Default.GetBytes(DateTime.Now.ToString("s"))));
+
+                    Console.WriteLine("message sent");
+
+                    tx.Complete();
+
+                    Console.WriteLine("tx completed");
+                }
+            }
+
+            await management.CloseAsync();
+            await sender.CloseAsync();
+        }
+    }
+}
