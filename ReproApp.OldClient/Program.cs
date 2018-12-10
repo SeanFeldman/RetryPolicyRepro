d4ecff3d02d479f5a80eeb5880e8e317a0ba56fc
Sean Feldman <feldman.sean@gmail.com>
2018-12-10 12:14:40 -0700

    Adding similar project for the old client - the old client didn't have retrys for TX scope

---
+++ ReproApp.OldClient/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ReproApp.OldClient
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString");
+
+            var management = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!await management.QueueExistsAsync("queue"))
+            {
+                await management.CreateQueueAsync("queue");
+            }
+
+            var sender = QueueClient.CreateFromConnectionString(connectionString, "queue");
+
+            while (true)
+            {
+                using (var tx = new TransactionScope(TransactionScopeOption.RequiresNew, TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    Debugger.Break();
+                    await sender.SendAsync(new BrokeredMessage(Encoding.Default.GetBytes(DateTime.Now.ToString("s"))));
+
+                    Console.WriteLine("message sent");
+
+                    tx.Complete();
+
+                    Console.WriteLine("tx completed");
+                }
+            }
+
+            await sender.CloseAsync();
+        }
+    }
+}
